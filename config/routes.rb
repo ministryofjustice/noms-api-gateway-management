f5d3a0c3a685bc4744e09c169f6045f64c577fa3
Himal Mandalia <himalmandalia@gmail.com>
2017-05-31 11:46:30 +0100

    Remove placeholder HomeController and set app root to AccessRequestsController#new

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'home#index'
-  get 'home/index'
+  root to: 'access_requests#new'
 
   resources :access_requests, except: [:edit, :update]
   resources :tokens
