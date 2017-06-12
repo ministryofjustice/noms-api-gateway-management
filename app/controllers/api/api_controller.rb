17aa550bef4318b4c18b97e09dabb6f7a3ced710
Himal Mandalia <himalmandalia@gmail.com>
2017-06-12 09:51:23 +0100

    Locks down endpoints under API controller, including revoked tokens endpoint.

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -1,9 +1,15 @@
 class Api::ApiController < ApplicationController
-  # before_action :authenticate!
+  before_action :authenticate!
 
   protected
 
   def authenticate!
-    # raise 'Unauthenticated'
+    raise 'Missing auth' if ENV['API_AUTH'].blank?
+
+    auth = params[:auth] || request.headers['Authorization']
+
+    if auth != ENV['API_AUTH']
+      render json: { error: 'Unauthorised'  }, status: 403
+    end
   end
 end
