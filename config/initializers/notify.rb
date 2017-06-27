70beaf55cd92f7049d2988d9df78154fc03bb295
Colin Saliceti <colin.saliceti@digital.justice.gov.uk>
2017-06-27 16:28:40 +0100

    Group notify environment config in initializers

---
+++ config/initializers/notify.rb
@@ -0,0 +1,4 @@
+Rails.configuration.govuk_notify_api_key = ENV.fetch('GOVUK_NOTIFY_API_KEY', '')
+Rails.configuration.access_request_notification_template = ENV['ACCESS_REQUEST_NOTIFICATION_TEMPLATE']
+Rails.configuration.token_trackback_template = ENV['TOKEN_TRACKBACK_TEMPLATE']
+Rails.configuration.team_email = ENV['TEAM_EMAIL']
