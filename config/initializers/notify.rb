c854bd71e77aa95c550f15553e98fccd9cabd0ad
Colin Saliceti <colin.saliceti@digital.justice.gov.uk>
2017-06-27 16:30:20 +0100

    Allow disabling Notify via env variable

--- config/initializers/notify.rb
+++ config/initializers/notify.rb
@@ -1,4 +1,8 @@
-Rails.configuration.govuk_notify_api_key = ENV.fetch('GOVUK_NOTIFY_API_KEY', '')
-Rails.configuration.access_request_notification_template = ENV['ACCESS_REQUEST_NOTIFICATION_TEMPLATE']
-Rails.configuration.token_trackback_template = ENV['TOKEN_TRACKBACK_TEMPLATE']
-Rails.configuration.team_email = ENV['TEAM_EMAIL']
+Rails.configuration.notify_enabled = ENV.fetch('NOTIFY_ENABLED', 'true').downcase != 'false'
+
+if Rails.configuration.notify_enabled {
+  Rails.configuration.govuk_notify_api_key = ENV.fetch('GOVUK_NOTIFY_API_KEY', '')
+  Rails.configuration.access_request_notification_template = ENV['ACCESS_REQUEST_NOTIFICATION_TEMPLATE']
+  Rails.configuration.token_trackback_template = ENV['TOKEN_TRACKBACK_TEMPLATE']
+  Rails.configuration.team_email = ENV['TEAM_EMAIL']
+}
