710e3795544d9034c95e23d2e08c04a91bea134d
Colin Saliceti <colin.saliceti@digital.justice.gov.uk>
2017-07-05 13:20:16 +0100

    Log when setting environment variables

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -10,10 +10,14 @@ cf api "${CF_API}"
 cf auth "${CF_USER}" "${CF_PASSWORD}"
 cf target -o "${CF_ORG}" -s "${CF_SPACE}"
 cf push --no-start "${CF_APP_NAME}"
+echo "Setting \$MOJSSO_ID variable..."
 cf set-env "${CF_APP_NAME}" MOJSSO_ID "${MOJSSO_ID}" > /dev/null
+echo "Setting \$MOJSSO_SECRET variable..."
 cf set-env "${CF_APP_NAME}" MOJSSO_SECRET "${MOJSSO_SECRET}" > /dev/null
+echo "Setting \$GOVUK_NOTIFY_API_KEY variable..."
 cf set-env "${CF_APP_NAME}" GOVUK_NOTIFY_API_KEY "${GOVUK_NOTIFY_API_KEY}" > /dev/null
-cf set-env "${CF_APP_NAME}" NOTIFY_ENABLED "${NOTIFY_ENABLED}" > /dev/null
+echo "Setting \$API_AUTH variable..."
 cf set-env "${CF_APP_NAME}" API_AUTH "${API_AUTH}" > /dev/null
-cf set-env "${CF_APP_NAME}" MOJSSO_URL "${MOJSSO_URL}" > /dev/null
+cf set-env "${CF_APP_NAME}" NOTIFY_ENABLED "${NOTIFY_ENABLED}"
+cf set-env "${CF_APP_NAME}" MOJSSO_URL "${MOJSSO_URL}"
 cf start "${CF_APP_NAME}"
