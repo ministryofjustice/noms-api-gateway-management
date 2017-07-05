1103bee4ed4fb757b7ddae949d94b8c985d7089c
Colin Saliceti <colin.saliceti@digital.justice.gov.uk>
2017-07-05 14:28:47 +0100

    Fix default AUTH_ENABLED variable

--- ci/test.sh
+++ ci/test.sh
@@ -6,5 +6,5 @@ SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
 
 docker run -v "${SCRIPT_DIR}"/..:/app -w /app \
   -e MOJSSO_USER="${MOJSSO_USER}" -e MOJSSO_PASSWORD="${MOJSSO_PASSWORD}" \
-  -e DOMAIN_UNDER_TEST="${DOMAIN_UNDER_TEST}" -e AUTH_ENABLED="${AUTH_ENABLED:-}" \
+  -e DOMAIN_UNDER_TEST="${DOMAIN_UNDER_TEST}" -e AUTH_ENABLED="${AUTH_ENABLED:-true}" \
   "${DOCKER_REGISTRY}"/nomis-api-access cucumber
