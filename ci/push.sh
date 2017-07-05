f8be7224aeb04cf088a315043bebd652f82ba87b
Colin Saliceti <colin.saliceti@digital.justice.gov.uk>
2017-07-05 13:22:15 +0100

    Fix docker run command

--- ci/push.sh
+++ ci/push.sh
@@ -7,5 +7,5 @@ docker run -v "${SCRIPT_DIR}"/..:/app -w /app \
   -e CF_USER -e CF_PASSWORD \
   -e NOTIFY_ENABLED -e GOVUK_NOTIFY_API_KEY \
   -e MOJSSO_ID -e MOJSSO_SECRET -e MOJSSO_URL \
-  -e API_AUTH
+  -e API_AUTH \
   governmentpaas/cf-cli ci/deploy.sh
