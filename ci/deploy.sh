c73e34c91c2218416bd8fb0aa5f61a615f0bb265
Colin Saliceti <colin.saliceti@digital.justice.gov.uk>
2017-07-05 13:30:41 +0100

    Conditionally set Notify variables

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -10,14 +10,23 @@ cf api "${CF_API}"
 cf auth "${CF_USER}" "${CF_PASSWORD}"
 cf target -o "${CF_ORG}" -s "${CF_SPACE}"
 cf push --no-start "${CF_APP_NAME}"
+
 echo "Setting \$MOJSSO_ID variable..."
 cf set-env "${CF_APP_NAME}" MOJSSO_ID "${MOJSSO_ID}" > /dev/null
+
 echo "Setting \$MOJSSO_SECRET variable..."
 cf set-env "${CF_APP_NAME}" MOJSSO_SECRET "${MOJSSO_SECRET}" > /dev/null
-echo "Setting \$GOVUK_NOTIFY_API_KEY variable..."
-cf set-env "${CF_APP_NAME}" GOVUK_NOTIFY_API_KEY "${GOVUK_NOTIFY_API_KEY}" > /dev/null
+
 echo "Setting \$API_AUTH variable..."
 cf set-env "${CF_APP_NAME}" API_AUTH "${API_AUTH}" > /dev/null
-cf set-env "${CF_APP_NAME}" NOTIFY_ENABLED "${NOTIFY_ENABLED}"
+
 cf set-env "${CF_APP_NAME}" MOJSSO_URL "${MOJSSO_URL}"
+
+if [ "${NOTIFY_ENABLED}" = "true" ]; then
+  cf set-env "${CF_APP_NAME}" NOTIFY_ENABLED "${NOTIFY_ENABLED}"
+
+  echo "Setting \$GOVUK_NOTIFY_API_KEY variable..."
+  cf set-env "${CF_APP_NAME}" GOVUK_NOTIFY_API_KEY "${GOVUK_NOTIFY_API_KEY}" > /dev/null
+fi
+
 cf start "${CF_APP_NAME}"
