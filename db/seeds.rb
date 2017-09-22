c492ecd416abfa6b35a496da3d93e869625e5312
Edward Andress <eddie_andress@hotmail.com>
2017-09-22 16:07:06 +0100

    Use %w(), not %(), for array of tokens in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 # Development seeds
 if Rails.env.development?
-  %(dev preprod prod).each do |env_name|
+  %w(dev preprod prod).each do |env_name|
     Environment.find_or_create_by!(name: env_name, provisioning_key: File.read(Rails.root.join("lib/assets/dummy_#{env_name}_provisioner.key")))
   end
 end
