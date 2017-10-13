a0fd2c2ec7a2f2df96b53d89527215854e13d090
Al Davidson <apdavidson@gmail.com>
2017-10-13 11:43:19 +0100

    refactor of ExceptionSafeResponseParser into inner classes

--- app/services/exception_safe_response_parser.rb
+++ app/services/exception_safe_response_parser.rb
@@ -3,50 +3,83 @@ class ExceptionSafeResponseParser
   attr_accessor :parsed_response, :logger
 
   def initialize
-    @parsed_response = OpenStruct.new(code: nil, data: nil)
-    log_file = Rails.root.join("./log/#{Rails.env}.log")
-    @logger = Logger.new(log_file)
+    @parsed_response = ParsedResponse.new(code: nil, data: nil)
   end
 
   def parse(response)
-    parsed_response.code = set_code(response)
-    parsed_response.data = set_data(response)
-    parsed_response
+    parsed_response.parse!(response)
   end
 
   private
 
-  def set_data(response)
-    response.is_a?(Exception) ? handle_exception(response) : parse_data(response.data)
-  end
+  class ParsedResponse
+    attr_accessor :code, :data, :logger
 
-  def parse_data(data)
-    data.is_a?(Hash) ? data : "#{parsed_response.code}#{': ' + data.truncate(30) if data }"
-  end
+    def initialize(attrs = {})
+      self.code = attrs[:code]
+      self.data = attrs[:data]
+      self.logger = Logger.new(STDERR)
+    end
 
-  def handle_exception(response)
-    logger.level = Logger::ERROR
-    logger.error(response)
+    def parse!(response)
+      self.code = status_code_for(response)
+      self.data = message_for(response)
+      self
+    end
 
-    case response
-    when SocketError
-      'Environment does not exist'
-    when OpenSSL::SSL::SSLError 
-      'SSL Error'
-    else
-      'Unexpected error'
+    protected
+
+    def message_for(response)
+      response.is_a?(Exception) ? exception_message(response) : parse_data(response.data)
+    end
+
+    def parse_data(data)
+      data.is_a?(Hash) ? data : "#{self.code}#{': ' + data.truncate(30) if data }"
+    end
+
+    def exception_message(response)
+      logger.level = Logger::ERROR
+      logger.error(response)
+
+      ResponseErrorAdapter.new(response).message
+    end
+
+    def status_code_for(response)
+      response.is_a?(Exception) ? code_for_exception(response) : response.raw_response.code.to_i
+    end
+
+    def code_for_exception(response)
+      ResponseErrorAdapter.new(response).code
     end
   end
 
-  def set_code(response)
-    response.is_a?(Exception) ? code_for_exception(response) : response.raw_response.code.to_i
-  end
+  class ResponseErrorAdapter
+    attr_accessor :code, :message
 
-  def code_for_exception(response)
-    case response
-      when SocketError then 404
-      when SSLError    then 525
-      else 'Unexpected error'
+    def initialize(response)
+      code_and_message = self.class.decorate(response)
+      self.code = code_and_message[:code]
+      self.message = code_and_message[:message]
+    end
+
+    protected
+
+    def self.codes_and_messages
+      {
+        'SocketError'            => {code: 404, message: 'Environment does not exist'},
+        'OpenSSL::SSL::SSLError' => {code: 525, message: 'SSL Error'}
+      }
+    end
+
+    def self.default_error
+      {
+        message: 'Unexpected error'
+      }
+    end
+
+    def self.decorate(response)
+      codes_and_messages[response.class.name] || default_error
     end
   end
+
 end
