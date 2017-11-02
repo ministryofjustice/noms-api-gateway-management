1b372b4e24709305759ffbac65974301715ffe95
Edward Andress <eddie_andress@hotmail.com>
2017-11-02 16:54:32 +0000

    Rename extensions.rb to delayed_job.rb

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -2,7 +2,6 @@ require 'delayed_job'
 
 module Delayed
   class Job
-
     def self.env_ids_in_queue
       queue.map do |job|
         job.job_data['arguments'].first.values.first.split('/').last.to_i
@@ -12,6 +11,5 @@ module Delayed
     def self.queue
       all.map { |job| YAML.load(job.handler) }
     end
-    
   end
 end
