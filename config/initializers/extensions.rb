66af58654183f2d0c0d8b6bba41c99be6929b17d
Edward Andress <eddie_andress@hotmail.com>
2017-11-07 11:22:25 +0000

    Revert "Rename extensions.rb to delayed_job.rb"
    This reverts commit 6f452ee9d2ce2cdd716660e8df8aec1e7f0e5f88.

--- config/initializers/extensions.rb
+++ config/initializers/extensions.rb
@@ -2,6 +2,7 @@ require 'delayed_job'
 
 module Delayed
   class Job
+
     def self.env_ids_in_queue
       queue.map do |job|
         job.job_data['arguments'].first.values.first.split('/').last.to_i
@@ -11,5 +12,6 @@ module Delayed
     def self.queue
       all.map { |job| YAML.load(job.handler) }
     end
+    
   end
 end
